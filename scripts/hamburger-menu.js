2b0f43f5d33e31268d64348439af08051cdb7cd6
mariussp415 <77110833+mariussp415@users.noreply.github.com>
2021-09-27 22:49:28 +0200

    la til js

---
+++ scripts/hamburger-menu.js
@@ -0,0 +1,16 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const headerContainer = document.querySelector(".header-container");
+
+const changeDisplay = () => {
+
+    const display = headerContainer.style.display;
+
+    if(display === "none" || display == ""){
+        headerContainer.style.display = "block";
+    }else {
+        headerContainer.style.display = "none";
+    }
+    
+}
+
+hamburgerMenu.addEventListener("click",changeDisplay);
